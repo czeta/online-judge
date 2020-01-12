00d7a5b013b5dc1138e04cac31c2a0ae74c25675
chenlongjie <757569797@qq.comm>
2020-01-12 15:15:12 +0800

    springboot initializr

---
+++ src/test/java/com/czeta/onlinejudge/OnlineJudgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.czeta.onlinejudge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineJudgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
