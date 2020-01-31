2facdb5f00bcaf324188c5c9eb8aded25eecc5fd
chenlongjie <757569797@qq.comm>
2020-01-31 15:36:16 +0800

    springboot基础脚手架

--- src/main/java/com/czeta/onlinejudge/AppBootstrap.java
+++ src/main/java/com/czeta/onlinejudge/AppBootstrap.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan({"com.czeta.oninejudge.**.mapper"})
+@MapperScan({"com.czeta.onlinejudge.**.mapper"})
 @SpringBootApplication
 public class AppBootstrap {
 
