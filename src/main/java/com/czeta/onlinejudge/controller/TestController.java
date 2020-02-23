11f331f2e32b845a5ac8769b5439382661aeac75
chenlongjie <757569797@qq.comm>
2020-02-23 13:57:43 +0800

    updated

--- src/main/java/com/czeta/onlinejudge/controller/TestController.java
+++ src/main/java/com/czeta/onlinejudge/controller/TestController.java
@@ -1,31 +1,18 @@
 package com.czeta.onlinejudge.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.shiro.authz.annotation.RequiresGuest;
-import org.apache.shiro.authz.annotation.RequiresRoles;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * @ClassName TestController
- * @Description 测试控制器
- * @Author chenlongjie
- * @Date 2020/2/23 12:52
- * @Version 1.0
- */
 @Slf4j
 @RestController
 public class TestController {
 
-    @GetMapping("/")
+    @RequestMapping("/")
     public String index() {
         log.info("test");
         return "index";
-    }
-
-    @RequiresGuest
-    @GetMapping("/test")
-    public String test() {
-        return "test";
     }
 }
